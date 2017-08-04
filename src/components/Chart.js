40e23374d696927016cffa366e1b08d076395385
Vishesh <visheshsingh16@gmail.com>
2017-08-04 11:45:37 -0400

    Rendering bar chart for mass population of New York

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,10 +2,49 @@ import React,  { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 
 export class Chart extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            chartData: {
+                labels: ["Yonkers", "Buffalo", "Albany", "Rochester", "Syracuse", "Binghamton", "Niagara Falls"],
+                datasets: [{
+                    label: 'Population',
+                    data: [195976, 261310, 97856, 210565, 145170, 47376, 50193],
+                    backgroundColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    borderColor: '#000',
+                    hoverBorderWidth:3,
+                    hoverBorderColor:'#000'
+                }]
+            }//chartData
+        }//this.state
+    }//constructor
     render() {
         return(
             <div className="Chart">
                 <h2>My Chart Component</h2>
+                <Bar
+                    data={this.state.chartData}
+                    options={{
+                        title: {
+                            display: true,
+                            text: 'Population by Cities in New York',
+                            fontSize: 25
+                        },
+                        legend: {
+                            display: true,
+                            position: 'right'
+                        }
+                    }}
+                    layout={{ }}
+                />
             </div>
         );
     }
