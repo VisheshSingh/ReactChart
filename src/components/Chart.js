b04cbc46b8446e8c29944b00742657696308b890
Vishesh <visheshsingh16@gmail.com>
2017-08-04 11:05:56 -0400

    Importing new component - Chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,1 +1,12 @@
+import React,  { Component } from 'react';
+import { Bar, Line, Pie } from 'react-chartjs-2';
 
+export class Chart extends Component {
+    render() {
+        return(
+            <div className="Chart">
+                <h2>My Chart Component</h2>
+            </div>
+        );
+    }
+}
